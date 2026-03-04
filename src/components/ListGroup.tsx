b03e49ebb4cf6178f7228b61e663fff805cadba0
snow-prog-14 <alejandramaraasin0@gmail.com>
2026-03-04 09:14:31 +0800

    handling event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,17 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New York", "Francisco", "Tokyo", "London"];
+
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={() => console.log("Clicked")}
-          >
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item} {}
           </li>
         ))}
