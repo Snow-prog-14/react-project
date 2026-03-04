0f5f2df3d9b2752d253ce87cf3498bb91a64a9a2
snow-prog-14 <alejandramaraasin0@gmail.com>
2026-03-04 08:53:20 +0800

    render

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,20 @@
-import { Fragment } from "react/jsx-runtime";
-
 function ListGroup() {
+  let items = ["New York", "Francisco", "Tokyo", "London"];
+
   return (
     <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        <h1>List</h1>
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log("Clicked")}
+          >
+            {item} {}
+          </li>
+        ))}
       </ul>
     </>
   );
